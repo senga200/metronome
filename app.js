32d21ac79722e33d7a7fe711baaacd0915c9dd09
Agnès Cappello <senga.ds@gmail.com>
2024-01-24 12:07:20 +0100

    footer

--- app.js
+++ app.js
@@ -85,12 +85,6 @@ signaturePlus.addEventListener("click", () => {
 //audioContext designe un graphe de traitement. il faut créer un audioContext avant de faire quoi que ce soit puisque tout passe dans un context. audioContext.destination permet de lier le son à une destination pour envoyer le son vers les hauts parleurs
 //un oscillateur permet de calculer les échantillons (tableaux d'intensité sonore)
 
-// const volumeSlider = document.querySelector(".volume");
-// const volumeMinus = document.querySelector(".volume-minus");
-// const volumePlus = document.querySelector(".volume-plus");
-// let gainNode1, gainNode2;
-// const oscillatorVolume = 0.5;
-
 let audioContext;
 let isPlaying = false;
 let intervalId;
@@ -104,12 +98,10 @@ function createOscillators() {
   const oscillator1 = audioContext.createOscillator();
   oscillator1.connect(audioContext.destination);
   oscillator1.frequency.value = 700; // Réglage frequence son
-  // oscillator1.connect(gainNode1);
 
   const oscillator2 = audioContext.createOscillator();
   oscillator2.connect(audioContext.destination);
   oscillator2.frequency.value = 950;
-  //oscillator2.connect(gainNode2);
 
   return { oscillator1, oscillator2 };
 }
@@ -139,17 +131,6 @@ function start() {
       createAudioContext();
     }
 
-    // // Créer les gainNodes pour le contrôle du volume
-    // gainNode1 = audioContext.createGain();
-    // gainNode1.connect(audioContext.destination);
-    // gainNode1.gain.value = 1;
-
-    // gainNode2 = audioContext.createGain();
-    // gainNode2.connect(audioContext.destination);
-    // gainNode2.gain.value = 1;
-
-    // intervalId permet de stocker l'id de l'interval pour pouvoir l'arreter avec clearInterval
-
     intervalId = setInterval(playClick, (60 / bpm) * 1000);
     isPlaying = true;
   }
@@ -169,28 +150,3 @@ startStop.addEventListener("click", () => {
     start();
   }
 });
-
-// // Event listener pour le contrôle de volume
-// volumeSlider.addEventListener("input", () => {
-//   gainNode1.gain.value = volumeSlider.value * oscillatorVolume;
-//   gainNode2.gain.value = volumeSlider.value * oscillatorVolume;
-//   volumeSlider.value = gainNode1.gain.value * 100 + "%";
-// });
-
-// volumeMinus.addEventListener("click", () => {
-//   if (gainNode1.gain.value <= 0) {
-//     return;
-//   }
-//   gainNode1.gain.value -= 0.1;
-//   gainNode2.gain.value -= 0.1;
-//   volumeSlider.value = gainNode1.gain.value * 100 + "%";
-// });
-
-// volumePlus.addEventListener("click", () => {
-//   if (gainNode1.gain.value >= 5) {
-//     return;
-//   }
-//   gainNode1.gain.value += 0.1;
-//   gainNode2.gain.value += 0.1;
-//   volumeSlider.value = gainNode1.gain.value * 100 + "%";
-// });
