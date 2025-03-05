c189e889b046c0177c06b38150726975ff18769b
Agnès Cappello <senga.ds@gmail.com>
2025-03-05 23:43:54 +0100

    fix-PWA

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then(() => console.log("Service Worker enregistré 🎉"))
-    .catch((err) => console.log("Erreur Service Worker 😢", err));
-}
+navigator.serviceWorker
+  .register("/service-worker.js")
+  .then(() => console.log("Service Worker enregistré 🎉"))
+  .catch((err) => console.log("Erreur Service Worker 😢", err));
 
 const tempoDisplay = document.querySelector(".tempo");
 const tempoMinus = document.querySelector(".tempo-minus");
